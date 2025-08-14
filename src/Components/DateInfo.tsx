c21b43cd5e00148f130a8002d55b7fd3ce511530
oussama ait moummad <ousamatessla@gmail.com>
2025-08-14 15:44:59 +0100

    add momentJs library to fox time

--- src/Components/DateInfo.tsx
+++ src/Components/DateInfo.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useContext } from "react";
+import moment from "moment-timezone";
 import { AppContext } from "../App";
 import jsonData from "../data/db.json";
 
@@ -12,67 +13,34 @@ type City = {
 
 const DateInfo = () => {
   const { city, setCity, apiData }: any = useContext(AppContext);
-  const [time, setTime] = useState(new Date());
+  const { EnTodayDate, ArTodayDate, Timezone } = apiData;
+  const { EnCityName, ArCityName, EnCountryName, ArCountryName, imgUrl } = city;
   const cities: City[] = jsonData.cities;
 
+  const [time, setTime] = useState(moment().tz(Timezone).format("HH:mm:ss"));
   useEffect(() => {
-    const interval = setInterval(() => setTime(new Date()), 1000);
+    const interval = setInterval(() => setTime(moment().tz(Timezone).format("HH:mm:ss")), 1000);
     return () => clearInterval(interval);
-  }, []);
-
-  const handleSelect = (cityName: string) => {
-    const selectedCity = cities.find((c) => c.EnCityName === cityName);
-    if (selectedCity) {
-      setCity(selectedCity);
-    }
-  };
-
-  const clock = time.toLocaleTimeString("en-GB", {
-    timeZone: apiData.Timezone,
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  });
+  }, [Timezone]);
+  
+  const handleSelect = (cityName: string) => setCity(cities.find(c => c.EnCityName === cityName));
 
   return (
-    <div
-      className="date_info text-light"
-      style={{
-        backgroundImage: `linear-gradient(#00000080, #00000080), url(${city.imgUrl})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+    <div className="date_info text-light" style={{ backgroundImage: `linear-gradient(#00000080, #00000080), url(${imgUrl})` }}>
       <div className="container h-100 position-relative d-flex">
         <div className="mt-md-auto w-100 d-flex flex-wrap">
           <ul className="list-unstyled me-auto">
-            <li>
-              <i className="text-uppercase fw-bold">{city.EnCityName}</i>,{" "}
-              {city.EnCountryName}
-            </li>
-            <li>{apiData.EnTodayDate}</li>
+            <li><i className="text-uppercase fw-bold">{EnCityName}</i>, {EnCountryName}</li>
+            <li>{EnTodayDate}</li>
           </ul>
           <ul className="list-unstyled text-end">
-            <li>
-              <i className="text-uppercase fw-bold">{city.ArCityName}</i>,{" "}
-              {city.ArCountryName}
-            </li>
-            <li>{apiData.ArTodayDate}</li>
+            <li><i className="text-uppercase fw-bold">{ArCityName}</i>, {ArCountryName}</li>
+            <li>{ArTodayDate}</li>
           </ul>
         </div>
-        <h1 className="clock position-absolute fw-bold w-100 text-center">
-          {clock}
-        </h1>
-        <select
-          className="form-select position-absolute"
-          value={city.EnCityName}
-          onChange={(e) => handleSelect(e.target.value)}
-        >
-          {cities.map((c, i) => (
-            <option key={i} value={c.EnCityName}>
-              {c.EnCityName}
-            </option>
-          ))}
+        <h1 className="clock position-absolute fw-bold w-100 text-center">{time}</h1>
+        <select className="form-select position-absolute" value={EnCityName} onChange={e => handleSelect(e.target.value)}>
+          {cities.map((c, i) => <option key={i} value={c.EnCityName}>{c.EnCityName}</option>)}
         </select>
       </div>
     </div>
